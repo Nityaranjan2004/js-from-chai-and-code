b22996375fe4637ee89f11ffef35ea60093b86ed
Nityaranjan2004 <nityaranjanjena6@gmail.com>
2026-03-02 16:15:59 +0530

    second commit

--- part 10/01_arrays.js
+++ part 10/01_arrays.js
@@ -64,7 +64,7 @@ const another_array = [1,2,3[4,5,6],7,[6,7[4,6]]];
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
 
-console.log("*********************");
+console.log("**********************");
 
 
 console.log(Array.from(nitya));
